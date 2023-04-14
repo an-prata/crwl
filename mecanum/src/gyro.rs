35ce110907f5128a82f402e1d81d9bf5729e2950
Evan Overman <evanrileyoverman@gmail.com>
2023-04-13 22:14:18 -0700

    pathetic start of gyro struct

---
+++ mecanum/src/gyro.rs
@@ -0,0 +1,10 @@
+// cwrl Copyright (c) 2023 Evan Overman (https://an-prata.it).
+// Licensed under the MIT License.
+// See LICENSE file in repository root for complete license text.
+
+pub struct Controller {
+    addr: u8,
+    yaw: f32,
+    roll: f32,
+    pitch: f32,
+}
